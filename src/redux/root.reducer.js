4b0054b42891b2b51e2b89b2a3cd7d33d11f9834
Fabian Trinler <fabian.trinler@blateral.com>
2016-06-20 16:34:17 +0200

    Adding basic albums reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux';
 
 import app from './app.reducer'
+import albums from './albums.reducer'
 
 export default combineReducers({
-    app
+    app,
+    albums
 })
