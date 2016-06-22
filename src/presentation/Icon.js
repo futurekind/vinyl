19f50ef67688a52eafc5d221d28c95f53b91925a
Fabian Trinler <fabian.trinler@blateral.com>
2016-06-22 17:41:33 +0200

    Adding Icon component

---
+++ src/presentation/Icon.js
@@ -0,0 +1,23 @@
+import React, {PropTypes as pt} from 'react';
+
+const Icon = (props) => (
+    <svg fill={props.fill} className={props.className} style={props.style} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        {props.children}
+    </svg>
+)
+
+Icon.defaultProps = {
+    fill: 'currentColor',
+    style: {
+        width: 24,
+        height: 24
+    }
+}
+
+Icon.propsTypes = {
+    fill: pt.string,
+    className: pt.string,
+    style: pt.object
+}
+
+export default Icon;
--- src/presentation/index.js
+++ src/presentation/index.js
@@ -1,9 +1,11 @@
 import Header from './Header';
 import Loader from './Loader';
 import Album from './Album';
+import Icon from './Icon';
 
 export {
     Header,
     Loader,
     Album
+    Icon
 }
