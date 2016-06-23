6e2de5e67e01577eeb184ba92580904460ad65fb
Fabian Trinler <fabian.trinler@blateral.com>
2016-06-23 12:28:10 +0200

    Adding dialog component

--- src/presentation/Dialog.js
+++ src/presentation/Dialog.js
@@ -9,9 +9,11 @@ const Dialog = (props) => {
         <div className={`${styles.view} ${props.open ? styles['is-open'] : ''}`}>
             <div className={styles.backdrop}></div>
             <div className={styles.content}>
-                <Icon>
-                    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-                </Icon>
+                <div className={styles.close}>
+                    <Icon onClick={props.onDismiss} className={styles.closeicon}>
+                        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+                    </Icon>
+                </div>
                 {props.children}
             </div>
         </div>
@@ -19,11 +21,13 @@ const Dialog = (props) => {
 }
 
 Dialog.defaultProps = {
-    open: false
+    open: false,
+    onDismiss: () => {}
 }
 
 Dialog.propTypes = {
-    open: pt.bool
+    open: pt.bool,
+    onDismiss: pt.func
 }
 
 export default Dialog;
