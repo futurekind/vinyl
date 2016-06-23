0da8ab31d95c682e5e0653d6da829f06b29c0097
Fabian Trinler <fabian.trinler@blateral.com>
2016-06-23 14:06:04 +0200

    Adding result list to AddDialog

--- src/container/AddDialog.js
+++ src/container/AddDialog.js
@@ -1,40 +1,69 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
-import {setAddDialogOpen} from '../redux/actions';
+import {setAddDialogOpen, searchApi} from '../redux/actions';
 import {Dialog, Searchfield, Loader} from '../presentation'
+
+import styles from './add-dialog.scss'
 
 class AddDialog extends Component {
     render() {
-        const {open, setOpen} = this.props;
+        const {open, setOpen, searching, results} = this.props;
 
         return (
             <Dialog open={open} onDismiss={() => setOpen(false)}>
+
                 <Searchfield
                     onSearch={this.handleSearch.bind(this)}
                     placeholder="Search for Artist or Album"
                 />
-                <Loader active={false} />
+
+                <div className={`${styles.results} ${results.length > 0 ? styles['is-active'] : ''}`}>
+                    {results.map(result => {
+                        return (
+                            <div key={result.collectionId} className={styles.result}>
+                                <div className={styles.cover}>
+                                    <img src={result.artworkUrl60} alt="" />
+                                </div>
+                                <div className={styles.titles}>
+                                    <div className={styles.artist}>{result.artistName}</div>
+                                    <div className={styles.album}>{result.collectionName}</div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+
+                <Loader active={searching} />
+
             </Dialog>
         )
     }
 
     handleSearch(e) {
         e.preventDefault();
+
+        const {onSearch} = this.props;
         const target = e.target;
         const searchterm = target.querySelector('[name="search"]').value
 
-        console.log(searchterm);
+        onSearch(searchterm);
     }
 }
 
 const mapState = state => ({
-    open: state.addDialog.isOpen
+    open: state.addDialog.isOpen,
+    searching: state.addDialog.isSearching,
+    results: state.addDialog.results
 })
 
 const mapDispatch = dispatch =>({
     setOpen(open) {
         dispatch(setAddDialogOpen(open))
+    },
+
+    onSearch(searchterm) {
+        dispatch(searchApi(searchterm))
     }
 })
 
