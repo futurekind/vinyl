4025a3979ad20014af8a330aee34e4c0a62458f4
Fabian Trinler <fabian.trinler@blateral.com>
2016-07-01 11:40:49 +0200

    Populating infos to DetailDialog

--- src/container/DetailDialog.js
+++ src/container/DetailDialog.js
@@ -6,6 +6,8 @@ import {getAlbumById} from '../redux/root.reducer';
 
 import {Dialog} from '../presentation';
 
+import style from './detail-dialog.scss';
+
 class DetailDialog extends Component {
 
     render() {
@@ -13,7 +15,15 @@ class DetailDialog extends Component {
 
         return (
             <Dialog open={open} onDismiss={this.handleDismiss.bind(this)}>
-
+                <div className={style.header}>
+                    <div className={style.header__cover}>
+                        <img src={album.cover} />
+                    </div>
+                    <div className={style.header__title}>
+                        <span className={style.title}>{album.title}</span>
+                        <span className={style.artist}>{album.artist}</span>
+                    </div>
+                </div>
             </Dialog>
         )
     }
@@ -29,7 +39,7 @@ class DetailDialog extends Component {
 const mapState = state => ({
     open: state.detailDialog.isOpen,
     detailId: state.app.activeDetail,
-    album: getAlbumById(state, state.app.activeDetail)
+    album: getAlbumById(state, state.app.activeDetail) || {}
 })
 
 const mapDispatch = dispatch => ({
