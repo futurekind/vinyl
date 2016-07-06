2ec41cdf4b5c7b2700aaa86ccbfd3e7e4bf665f7
Fabian Trinler <fabian.trinler@blateral.com>
2016-07-06 17:26:23 +0200

    Adding Icons instead of text

--- src/container/App.js
+++ src/container/App.js
@@ -6,7 +6,7 @@ import AddDialog from './AddDialog';
 import DetailDialog from './DetailDialog';
 import Main from './Main';
 import Filters from './Filters';
-import {Header, Loader} from '../presentation';
+import {Header, Loader, Icon} from '../presentation';
 import {setAddDialogOpen, setFilterSettingsOpen} from '../redux/actions';
 
 import styles from './app.scss';
@@ -16,15 +16,27 @@ class App extends Component {
     render() {
         const {isFetching, dialogAddOpen, setAddDialogOpen, activeDetailId, filterSettingsOpen, onSetFilterSettingsOpen} = this.props;
 
+        const rightIcon = (
+            <Icon>
+                <path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9h-4v4h-2v-4H9V9h4V5h2v4h4v2z"/>
+            </Icon>
+        )
+
+        const leftIcon = (
+            <Icon>
+                <path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z"/>
+            </Icon>
+        )
+
         return (
             <div className={styles.app}>
 
                 <div className={`${styles.main} ${filterSettingsOpen ? styles['main--open'] : ''}`}>
                     <Header
                         title="Recordman"
-                        rightAction="Add"
+                        rightAction={rightIcon}
                         onRightActionClick={() => setAddDialogOpen(true)}
-                        leftAction="Filter"
+                        leftAction={leftIcon}
                         onLeftActionClick={() => onSetFilterSettingsOpen(!filterSettingsOpen)}
                     />
 
@@ -33,7 +45,7 @@ class App extends Component {
                         <AlbumsList />
                     </Main>
                 </div>
-                
+
                 <Filters />
                 <AddDialog />
                 <DetailDialog />
