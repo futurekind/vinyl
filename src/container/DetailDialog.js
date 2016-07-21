40946efddb66c571f65bc5207d873bc1b85dc943
futurekind <f.trinler@gmail.com>
2016-07-22 00:01:54 +0200

    [wip] open in apple music

--- src/container/DetailDialog.js
+++ src/container/DetailDialog.js
@@ -42,6 +42,7 @@ class DetailDialog extends Component {
                 </div>
                 <div className={style.footer}>
                     <div className={style.footer__left}>
+                        <button onClick={this.handleAppleMusicClick.bind(this)}>Foo</button>
                         <div className={`${style.btn} ${album.category === 1 ? style.active : ''}`}  onClick={this.handleSetCategory.bind(this, 1)}>
                             <Icon><path d="M17 20c-.29 0-.56-.06-.76-.15-.71-.37-1.21-.88-1.71-2.38-.51-1.56-1.47-2.29-2.39-3-.79-.61-1.61-1.24-2.32-2.53C9.29 10.98 9 9.93 9 9c0-2.8 2.2-5 5-5s5 2.2 5 5h2c0-3.93-3.07-7-7-7S7 5.07 7 9c0 1.26.38 2.65 1.07 3.9.91 1.65 1.98 2.48 2.85 3.15.81.62 1.39 1.07 1.71 2.05.6 1.82 1.37 2.84 2.73 3.55.51.23 1.07.35 1.64.35 2.21 0 4-1.79 4-4h-2c0 1.1-.9 2-2 2zM7.64 2.64L6.22 1.22C4.23 3.21 3 5.96 3 9s1.23 5.79 3.22 7.78l1.41-1.41C6.01 13.74 5 11.49 5 9s1.01-4.74 2.64-6.36zM11.5 9c0 1.38 1.12 2.5 2.5 2.5s2.5-1.12 2.5-2.5-1.12-2.5-2.5-2.5-2.5 1.12-2.5 2.5z"/></Icon>
                             <span>Listen</span>
@@ -51,6 +52,7 @@ class DetailDialog extends Component {
                             <Icon><path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/></Icon>
                             <span>Buy</span>
                         </div>
+
                     </div>
 
                     <div className={style.footer__right}>
@@ -103,6 +105,11 @@ class DetailDialog extends Component {
         const {album, onSetAlbumCategory} = this.props;
 
         onSetAlbumCategory(album.id, category);
+    }
+
+    handleAppleMusicClick() {
+        const {album} = this.props;
+        window.open(album.url);
     }
 }
 
