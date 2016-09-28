24da819523e6a0e15c8dfce0d992b04491eabf55
Fabian Trinler <fabian.trinler@blateral.com>
2016-09-28 10:49:03 +0200

    Adding sorting options

--- src/container/Filters.js
+++ src/container/Filters.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
-import {setFilterSettingsOpen, setActiveFilter} from '../redux/actions';
+import {setFilterSettingsOpen, setActiveFilter, setActiveSort} from '../redux/actions';
 
 import style from './filters.scss';
 
 class Filters extends Component {
     render() {
-        const {open, onSetFilterSettingsOpen, activeFilter} = this.props;
+        const {open, onSetFilterSettingsOpen, activeFilter, activeSort} = this.props;
 
         return (
             <div className={`${style.view} ${open ? style['is-open'] : ''}`} >
@@ -18,7 +18,14 @@ class Filters extends Component {
                     <li onClick={this.handleSetActiveFilter.bind(this, 0)} className={`${activeFilter === 0 ? style.active : ''}`}>Show All</li>
                     <li onClick={this.handleSetActiveFilter.bind(this, 1)} className={`${activeFilter === 1 ? style.active : ''}`}>Show Listen</li>
                     <li onClick={this.handleSetActiveFilter.bind(this, 2)} className={`${activeFilter === 2 ? style.active : ''}`}>Show Buy</li>
+
+                    <li className={style.sep} />
+
+                    <li className={`${style.sort}  ${activeSort === 0 ? style.active : ''}`} onClick={this.handleSetActiveSort.bind(this, 0)}>Sort by Added At</li>
+                    <li className={`${style.sort}  ${activeSort === 1 ? style.active : ''}`} onClick={this.handleSetActiveSort.bind(this, 1)}>Sort by Artist</li>
+                    <li className={`${style.sort}  ${activeSort === 2 ? style.active : ''}`} onClick={this.handleSetActiveSort.bind(this, 2)}>Sort by Album Title</li>
                 </ul>
+
             </div>
         )
     }
@@ -32,11 +39,18 @@ class Filters extends Component {
             onSetFilterSettingsOpen(false);
         }, 100)
     }
+
+    handleSetActiveSort(sort) {
+        const { onSetActiveSort } = this.props;
+
+        onSetActiveSort(sort)
+    }
 }
 
 const mapState = state => ({
     open: state.filters.open,
-    activeFilter: state.filters.activeFilter
+    activeFilter: state.filters.activeFilter,
+    activeSort: state.filters.activeSort,
 })
 
 const mapDispatch= dispatch => ({
@@ -46,6 +60,10 @@ const mapDispatch= dispatch => ({
 
     onSetActiveFilter(filter) {
         dispatch(setActiveFilter(filter))
+    },
+
+    onSetActiveSort(sort) {
+        dispatch(setActiveSort(sort))
     }
 })
 
