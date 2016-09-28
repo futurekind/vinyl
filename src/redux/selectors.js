674d51bb232110585b8dfffb0052a04cd9796b94
Fabian Trinler <fabian.trinler@blateral.com>
2016-09-28 11:18:37 +0200

    Adding selectors to sort and filter albums

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,7 @@
 import { createSelector } from 'reselect';
 import moment from 'moment';
 
-const albumsSelector = state => state.albums.sort((a, b) => {
+const sortAlbumsByAddedAt = albums => albums.sort((a, b) => {
     const aAddedtAt = moment(a.addedAt);
     const bAddedtAt = moment(b.addedAt);
 
@@ -14,20 +14,46 @@ const albumsSelector = state => state.albums.sort((a, b) => {
     }
 
     return 0;
-});
+})
 
+const sortAlbumsByArtist = albums => albums.sort((a, b) => {
+    if(a.artist < b.artist) return -1;
+    return 1;
+})
+
+const sortAlbumsByTitle = albums => albums.sort((a, b) => {
+    if(a.title < b.title) return -1;
+    return 1
+})
+
+const albumsSelector = state => state.albums;
 const activeFilterSelector = state => state.filters.activeFilter;
 const activeDetailId = state => state.app.activeDetail
+const activeSortSelector = state => state.filters.activeSort;
 
 export const getAllAlbums = createSelector(
     albumsSelector,
     activeFilterSelector,
+    activeSortSelector,
 
-    (albums, activeFilter) => {
-        
-        if(activeFilter === 0) return albums;
+    (albums, activeFilter, activeSort) => {
+        let sorted;
 
-        return albums.filter(album => album.category === activeFilter)
+        switch(activeSort) {
+            case 1:
+                sorted = [...sortAlbumsByArtist(albums)];
+                break;
+            case 2:
+                sorted = [...sortAlbumsByTitle(albums)];
+                break;
+            default: 
+                sorted = [...sortAlbumsByAddedAt(albums)]
+        }
+
+        if(activeFilter === 0) return sorted;
+
+        return sorted
+                .filter(album => album.category === activeFilter)
     }
 )
 
